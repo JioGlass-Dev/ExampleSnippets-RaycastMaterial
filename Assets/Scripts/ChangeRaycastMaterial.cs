cacec5e9b51ae056b3c5de7acff43b33039a4a64
Shreyansh Anchlia <developer@tesseract.in>
2021-11-08 17:26:27 +0530

    Added example for Raycast Material changing

---
+++ Assets/Scripts/ChangeRaycastMaterial.cs
@@ -0,0 +1,37 @@
+﻿using JMRSDK.InputModule;
+using JMRSDK.UX;
+using UnityEngine;
+
+namespace JMRSDKExampleSnippets
+{
+    /// <summary>
+    /// Changes Raycast Material to the focused object and select button is triggered.
+    /// </summary>
+    public class ChangeRaycastMaterial : MonoBehaviour, ISelectClickHandler
+    {
+        /// <summary>
+        /// PointerLaserUnity holds reference to the material of raycast line renderer
+        /// </summary>
+        [HideInInspector] public PointerLaserUnity laser;
+
+        /// <summary>
+        /// On select button click assign the focused object material to the raycast material.
+        /// </summary>
+        /// <param name="eventData">
+        /// Data related to select click event
+        /// </param>
+        public void OnSelectClicked(SelectClickEventData eventData)
+        {
+            if (laser == null)
+            {
+                laser = FindObjectOfType<PointerLaserUnity>();
+                if (laser == null)
+                {
+                    Debug.LogError("Pointer Laser Missing!");
+                    return;
+                }
+            }
+            laser.LineMaterial = this.GetComponent<MeshRenderer>().material;
+        }
+    }
+}
